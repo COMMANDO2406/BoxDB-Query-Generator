1eba74763eccc80908b298281c8f375ca7ff86a9
COMMANDO2406 <raghavsethi.in@gmail.com>
2024-10-21 16:47:44 +0530

    added credential storage

--- credentials.js
+++ credentials.js
@@ -1,8 +1,25 @@
 function credentialsUpdate() {
   const textArea = document.getElementById("textArea");
-
-  textArea.setAttribute(
-    "placeholder",
-    "Enter Credentials in USER PASSWORD HOST DATABASE"
+  const credentials = prompt(
+    "Enter credentials in format: USER PASSWORD HOST DATABASE"
   );
+
+  if (credentials) {
+    const [user, password, host, database] = credentials.split(" ");
+
+    if (user && password && host && database) {
+      alert(
+        `User: ${user}\nPassword: ${password}\nHost: ${host}\nDatabase: ${database}`
+      );
+
+      localStorage.setItem("user", user);
+      localStorage.setItem("password", password);
+      localStorage.setItem("host", host);
+      localStorage.setItem("database", database);
+
+      textArea.value = `Credentials \nUser: ${user}\nPassword: ${password}\nHost: ${host}\nDatabase: ${database}`;
+    } else {
+      alert("Please enter all four values: USER PASSWORD HOST DATABASE");
+    }
+  }
 }
